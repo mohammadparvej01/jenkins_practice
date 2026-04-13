6cd7824056840880beb39835a8486f1ce00ddf81
Mohammad Parvej Mohammad Wajid <mohammad_parvejmohammadwajid@epam.com>
2026-04-13 17:14:34 +0530

    updated to check breaking of code

--- DemoApi/Program.cs
+++ DemoApi/Program.cs
@@ -14,6 +14,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+int x = "op";  // to check breaking of code using jenkins
+
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
